55d8ce7139b5f6137a36cd0cb7f8a79704585b18
Hardroners <66667081+hardroners@users.noreply.github.com>
2024-04-23 10:01:40 +0200

    Add files via upload

---
+++ 14.Le-JavaScript-asynchrone/1.setTimeout-setInterval/script.js
@@ -0,0 +1,33 @@
+/* 
+    Les méthodes setTimeout() et setInterval() sont très utilisées en JavaScript.
+    
+    Elles permettent d'exécuter une fonction callback de manière différée dans le temps, sans polluer le fil principal d'exécution JS.
+
+    Ces opérations sont dîtes asynchrones.
+*/
+
+/*  
+1. window.setTimeout(callback, delay).
+
+Valeur de retour : un id permettant de stopper le timeout si on le souhaite avec clearTimeout().
+
+Exécute une callback ou bout d'un certain délai en millisecondes.
+*/
+
+const timeoutID = setTimeout(() => {
+    console.log("Log au bout de 2s")
+}, 2000)
+clearTimeout(timeoutID)
+
+
+/* 
+    2. window.setInterval(callback, delay)
+
+    Valeur de retour : Un id permettant de mettre fin au setInterval.
+
+    Fonctionnement : Exécute une fonction callback tous les x millisecondes définies par le second argument.
+*/
+const intervalID = setInterval(() => {
+    console.log("Log au bout de 2s")
+}, 2000)
+clearTimeout(intervalID)
---
+++ 14.Le-JavaScript-asynchrone/2.Les-erreurs/script.js
@@ -0,0 +1,47 @@
+/*
+  Plusieurs types d'erreurs existent de base en JavaScript.
+  Lorsqu'elles ont lieu, elles bloquent l'exécution totale du script et retournent une erreur.
+
+  On peut envoyer des erreurs de différentes manières
+  
+  throw *expression* 
+  
+  throw Error(*expression*) // fonction déclenchant une erreur et retournant un objet contenant l'expression plus une information sur l'origine de l'erreur
+
+  throw new Error(*expression*) // Similaire à throw Error() mais étant appelée sous la form d'un constructeur
+*/
+
+function foo(){
+  console.log("FOO")
+  // throw Error("Abc")
+}
+
+// foo()
+// console.log("Après foo")
+// console.log("Après foo")
+// console.log("Après foo")
+
+/*
+    Il existe une série d'instructions permettant de tester du code et de gérer les erreurs si elles ont lieu.
+    C'est le try...catch...finally
+    Si une erreur a lieu dans le bloc try{}, elle déclenchera l'exécution du bloc catch{} et n'anulera pas l'exécution du reste du script.
+
+    On pourra également récupérer les données de l'erreur.
+ */
+
+try {
+  let a = 555
+  if(typeof a !== "string") throw Error("Erreur")
+}
+catch(e) {
+  console.log(e)
+}
+finally{
+  console.log("Je suis executé quoiqu'il arrive")
+}
+
+// Les instructions try catch permettent de ne pas bloquer l'exécution du reste du code s'il y a une erreur.
+// console.log("After try catch")
+// console.log("After try catch")
+// console.log("After try catch")
+// console.log("After try catch")
